160bea97b7dfe10c31e5046177e3b376b4443a15
Shugu <cristiancsete@yahoo.com>
2024-01-30 23:45:32 +0000

    Initial Setup of MAUI application

---
+++ Tempus/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Tempus;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ Tempus/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Tempus;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
